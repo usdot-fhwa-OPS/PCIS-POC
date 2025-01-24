d63f9d4ddd685d27a834d7f7c2402de0a03c8130
christopher.h.castillo@leidos.com <christopher.h.castillo@leidos.com>
2025-01-24 14:31:04 -0500

    undid some changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.tsx'
 import Auth from './components/authenticator/authenticator.tsx'
-
-
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
