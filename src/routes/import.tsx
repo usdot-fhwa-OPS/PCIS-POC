b8d98049f8b306fb03e8513ee1fe315a702292bb
ChrisC027 <christopher.h.castillo@leidos.com>
2025-02-05 09:23:16 -0500

    integrated Amplify Storage and configured S3 bucket

--- src/routes/import.tsx
+++ src/routes/import.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { FileUploader } from '@aws-amplify/ui-react-storage';
+import '@aws-amplify/ui-react/styles.css';
 
 export const Route = createFileRoute('/import')({
   component: RouteComponent,
@@ -6,8 +8,31 @@ export const Route = createFileRoute('/import')({
 
 function RouteComponent() {
   return (
-    <div className="p-2">
-      <h3>Import BAPLIE Page</h3>
-    </div>
+    <main className="p-6">
+      <div className="max-w-2xl">
+        <h1 className="text-2xl font-semibold tracking-tight">How to import BAPLIE DATA</h1>
+        <ol className="mt-6 space-y-4 list-decimal list-inside text-sm">
+          <li>Locate your BAPLIE file on your computer (ensure it&apos;s in .edi or .txt format).</li>
+          <li>Drag and drop the file into the upload area or click &quot;Select File&quot; to browse for it.</li>
+          <li>Ensure the file name appears in the list with a success indicator.</li>
+          <li>Click &quot;Process Data&quot; to validate and import the dataset.</li>
+        </ol>
+
+        <div className="mt-8">
+          <h2 className="text-xl font-semibold tracking-tight">Start Import BAPLIE DATA</h2>
+          <p className="mt-2 text-sm text-muted-foreground">
+            Upload and process your BAPLIE file for your container planning and management.
+          </p>
+          <FileUploader
+            acceptedFileTypes={[
+              '.csv',
+            ]}
+            path="stowPlans/"
+            maxFileCount={1}
+            isResumable
+          />
+        </div>
+      </div>
+    </main>
   )
 }
